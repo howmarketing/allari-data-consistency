08c44dd455087802fa5e32707756ea98fb6972c9
Gabriel Ariza Gomes de Castro <gabsariza@icloud.com>
2024-05-13 23:00:25 -0300

    feat(v2-implement-peewee): saving the src/lib/database/migration/migration.py progress

--- src/lib/database/migration/migration.py
+++ src/lib/database/migration/migration.py
@@ -244,17 +244,89 @@ class FindExperiences:
         )
 
 
-
-        
+class FindExperiencesWithPermanenceDays:
+    
+    __query = ""
+    __query_result = []
+    person_id = 0
+    min_permanence_days = 0
+    
+    def __init__(self, person_id = 0, min_permanence_days = 90):
+        self.person_id = person_id
+        self.min_permanence_days = min_permanence_days
+    
+    def set_query(self):
+        self.__query = ""
+        self.__query +=    "SELECT  "
+        self.__query +=        "e.id, "
+        self.__query +=        "e.person_id, "
+        self.__query +=        "e.company, "
+        self.__query +=        "e.title, "
+        self.__query +=        "(JULIANDAY(COALESCE(end_date, CURRENT_DATE)) - JULIANDAY(start_date)) as 'permanence_days', "
+        self.__query +=        "e.start_date, "
+        self.__query +=        "e.end_date "
+        self.__query +=    "FROM  "
+        self.__query +=        "experience as e "
+        self.__query +=    "WHERE "
+        self.__query +=        "person_id >= ?"
+        self.__query +=    "AND "
+        self.__query +=        "permanence_days >= ?"
+        self.__query +=     "ORDER BY "
+        self.__query +=         "e.id, e.person_id ASC"
+    
+    def execute_query(self):
+        cursor = db.execute_sql('{}'.format(self.__query), (self.person_id, self.min_permanence_days))
+        for value in cursor:
+            id, person_id, company, title, permanence_days, start_date, end_date = value
+            data = {
+                'id'                    :       id,
+                'person_id'             :       person_id,
+                'company'               :       company,
+                'title'                 :       title,
+                'permanence_days'       :       permanence_days,
+                'start_date'            :       start_date,
+                'end_date'              :       end_date
+            }
+            self.__query_result.append(data)
+        return self.__query_result
+    
+    def get_query_result(self):
+        self.set_query()
+        self.execute_query()
+        return self.__query_result
+   
+def print2(data):
+    # Print the param __name__ and __value__
+    print(getattr(data, '__name__', '') or '')
+    try:
+        if isinstance(data, list):
+                print(json.dumps(data, indent=2))
+        else:
+            print(json.dumps([data], indent=2))
+    except Exception as e:
+        print(data)
 PERSON_ID = 1
+
 
 def migrate_database():
     migration = Migration()
-    # find_contacts_by_person_id = FindContactsByPersonId(PERSON_ID)
-    # person_contacts = find_contacts_by_person_id.get_query_result()
-    person_contacts = FindContactsByPersonId(PERSON_ID).get_query_result()
     
-    print(person_contacts)
+    
+    # person_contacts = FindContactsByPersonId(PERSON_ID).get_query_result(True)
+    person_contacts = FindContactsByPersonId(PERSON_ID).get_query_result(False)
+    print2(person_contacts)
+    
+    
+    # experiences_with_permanence_days = FindExperiencesWithPermanenceDays(person_id=PERSON_ID).get_query_result()
+    experiences_with_permanence_days = FindExperiencesWithPermanenceDays(0, 2000).get_query_result()
+    print2(experiences_with_permanence_days)
+    
+    
+    
+    
+    
+    
+    
     
     
     return
@@ -268,6 +340,8 @@ def migrate_database():
     
     
     
+    # find_contacts_by_person_id = FindContactsByPersonId(PERSON_ID)
+    # person_contacts = find_contacts_by_person_id.get_query_result()
     find_experiences = FindExperiences()
     person_experiences:list[Experience] = find_experiences.get_experiences_by_person_id(PERSON_ID)
     persons_data = migration.get_person_data()
