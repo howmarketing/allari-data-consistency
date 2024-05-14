f157a878e46f108bd6918697dd8b90c17dfa1ea8
Gabriel Ariza Gomes de Castro <gabsariza@icloud.com>
2024-05-14 19:23:09 -0300

    # [V4] FEATURE | CLEAR CODE BASE
    ## TODO
    
    - [ ] 1. Clear the code-base;
    
    ### DOING
    
     - 1. Clear the code-base;

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from src.lib.database.migration.migration_test import execute
-def main():
-    execute()
+from src.services.person_relationships import execute
+def main(person_id=0):
+    execute(person_id)
     
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    import sys
+    person_id = int(sys.argv[1])
+    main(person_id)
