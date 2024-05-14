50720d5fcc34cf39ef2a4831c9f06b16d888047f
Gabriel Ariza Gomes de Castro <gabsariza@icloud.com>
2024-05-14 19:57:29 -0300

    # [V4] FEATURE | CLEAR CODE BASE
    ## TODO
    
    - [ ] 1. Clear the code-base;
    
    ### DOING
    
     - 1. Clear the code-base;

--- set-up.sh
+++ set-up.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 # Initialize conda in your current shell
-eval "$(conda shell.bash hook)"
-
-conda create --name ORM_TEST python=3.9.6
-conda activate ORM_TEST
-conda install --file requirements.txt
-
-conda env config vars set PYTHONPATH="/Users/gabrielariza/Documents/allari/allari-data-consistency"
-
-
-conda list
+conda create --name v4 python=3.9.6
+conda activate v4
+pip install -r requirements.txt
